03e587400cdba4410cf7bfd515802511927bf278
aj-ya <ajeyabhat.off@gmail.com>
2022-01-21 19:40:44 +0530

    init

--- scripts/kube.sh
+++ scripts/kube.sh
@@ -1,6 +1,7 @@
 sudo modprobe br_netfilter
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
+
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
@@ -8,9 +9,10 @@ EOF
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-net.bridge.ipv4.ip_forwar = 1
+net.bridge.ipv4.ip_forward = 1
 EOF
 sudo sysctl --system
+
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -19,13 +21,21 @@ enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
 EOF
 
 # Set SELinux in permissive mode (effectively disabling it)
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
-sudo yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
-sudo systemctl enable docker
-sudo systemctl start docker
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
 sudo systemctl enable --now kubelet
+# Set SELinux in permissive mode (effectively disabling it)
+# sudo setenforce 0
+# sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# sudo yum install -y docker kubelet kubeadm kubectl --disableexcludes=kubernetes
+# sudo systemctl enable docker
+# sudo systemctl start docker
+# sudo systemctl enable --now kubelet
