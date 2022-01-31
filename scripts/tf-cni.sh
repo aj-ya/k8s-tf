09a4cbb88850905e2223f0b22815a5e93d5806de
aj-ya <ajeyabhat.off@gmail.com>
2022-01-31 13:53:10 +0530

    change in tf

--- scripts/tf-cni.sh
+++ scripts/tf-cni.sh
@@ -1,7 +1,6 @@
-#!bin/sh
 K8S_MASTER_IP=x.x.x.x;
 UNDERLAY_IP=x.x.x.x;
-CONTRAIL_REPO="docker.io\/opencontrailnightly";
+CONTRAIL_REPO="tungstenfabric";
 CONTRAIL_RELEASE="latest";
 sudo mkdir -pm 777 /var/lib/contrail/kafka-logs;
-curl https://raw.githubusercontent.com/Juniper/contrail-kubernetes-docs/master/install/kubernetes/templates/contrail-single-step-cni-install-centos.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/{{ CONTRAIL_REPO }}/$CONTRAIL_REPO/g; s/{{ CONTRAIL_RELEASE }}/$CONTRAIL_RELEASE/g; s/VROUTER_GATEWAY:$K8S_MASTER_IP /VROUTER_GATEWAY: $UNDERLAY_IP/g" > tf_cni.yaml
+curl https://raw.githubusercontent.com/aj-ya/k8s-tf/main/tf-config-old-mod.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/{{ CONTRAIL_REPO }}/$CONTRAIL_REPO/g; s/{{ CONTRAIL_RELEASE }}/$CONTRAIL_RELEASE/g; s/VROUTER_GATEWAY:$K8S_MASTER_IP /VROUTER_GATEWAY: $UNDERLAY_IP/g" > tf_cni.yaml
