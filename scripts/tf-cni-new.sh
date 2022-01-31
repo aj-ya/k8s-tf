85763f52b69ea15ee07c17cd55c8b9a7c3937a14
Ajeya Bhat <59968173+aj-ya@users.noreply.github.com>
2022-01-31 13:28:33 +0530

    Update tf-cni-new.sh

--- scripts/tf-cni-new.sh
+++ scripts/tf-cni-new.sh
@@ -2,4 +2,4 @@
 K8S_MASTER_IP=x.x.x.x;
 UNDERLAY_IP=x.x.x.x;
 sudo mkdir -pm 777 /var/lib/contrail/kafka-logs;
-curl https://raw.githubusercontent.com/aj-ya/k8s-tf/master/tf.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/VROUTER_GATEWAY:{{ VROUTER_GATEWAY }} /VROUTER_GATEWAY: $UNDERLAY_IP/g" | kubectl apply -f 
+curl https://raw.githubusercontent.com/aj-ya/k8s-tf/master/tf.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/{{ VROUTER_GATEWAY }}/$UNDERLAY_IP/g" | kubectl apply -f 
