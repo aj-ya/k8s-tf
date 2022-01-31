a9c5087c29df8dbca3ea8b0b8d86503c11b0fb90
aj-ya <ajeyabhat.off@gmail.com>
2022-01-31 14:20:41 +0530

    change in tf

--- scripts/tf-cni.sh
+++ scripts/tf-cni.sh
@@ -3,4 +3,4 @@ UNDERLAY_IP=x.x.x.x;
 CONTRAIL_REPO="tungstenfabric";
 CONTRAIL_RELEASE="latest";
 sudo mkdir -pm 777 /var/lib/contrail/kafka-logs;
-curl https://raw.githubusercontent.com/aj-ya/k8s-tf/main/tf-config-old-mod.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/{{ CONTRAIL_REPO }}/$CONTRAIL_REPO/g; s/{{ CONTRAIL_RELEASE }}/$CONTRAIL_RELEASE/g; s/VROUTER_GATEWAY:$K8S_MASTER_IP /VROUTER_GATEWAY: $UNDERLAY_IP/g" > tf_cni.yaml
+curl https://raw.githubusercontent.com/aj-ya/k8s-tf/main/tf-config-old-mod.yaml | sed "s/{{ K8S_MASTER_IP }}/$K8S_MASTER_IP/g; s/{{ CONTRAIL_REPO }}/$CONTRAIL_REPO/g; s/{{ CONTRAIL_RELEASE }}/$CONTRAIL_RELEASE/g; s/VROUTER_GATEWAY:$K8S_MASTER_IP/VROUTER_GATEWAY: $UNDERLAY_IP/g" > tf_cni.yaml
